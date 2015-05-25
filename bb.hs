ff81ded307edc939cfa631477cfcd6612ad1f9cd
SRomansky <sk.romansky@gmail.com>
2015-05-24 22:38:16 -0600

    Added error handling for resolving hostname to IP address

--- bb.hs
+++ bb.hs
@@ -1,59 +1,90 @@
+import Control.Exception
+import Control.Monad (liftM, guard)
+import Network.BSD
+import Network.Socket
 import System.Exit
+import System.IO.Error
 
 -- based on RFC 2812: https://tools.ietf.org/html/rfc2812
 
 -- RFC2812;2.3.1: TCP/IP most common underlying network protocol.
+-- This is an ipv4 bot I guess...
 
 data Server = Server
-    { hostname  :: String
-    , port      :: String
+    { hostname  :: HostName
+    , port      :: PortNumber
     , tls       :: Bool
-    }
-  deriving (Show)
-
-data Bot = Bot
-    { nicks     :: [String]
+    , nicks     :: [String]
     , username  :: String
     , realname  :: String
     , usermode  :: Integer
     , channels  :: [String]
     }
-
+  deriving (Show)
 
 -- Default server variables, TODO put these into a config file and read them.
 s = Server
-    { hostname = "localhost"
-    , port = "6667"
+    { hostname = "localhost" :: HostName
+    , port = 6667
     , tls = False
-    }
-
--- Default bot variables
-b = Bot
-    { nicks    = ["BottoB", "toB", "Bot12345"]
+    , nicks    = ["BottoB", "toB", "Bot12345"]
     , username = "bot"
     , realname = "bot"
     , usermode = 0
     , channels = ["#testset"]
     }
 
+
+
+-- True if success
+connectToServer :: Server -> IO ()
+connectToServer ser = do
+  sock <- socket AF_INET Stream defaultProtocol
+  r <- tryJust (guard . isDoesNotExistError) $
+                      liftM hostAddress . getHostByName $ hostname ser
+  case r of
+    Left e -> putStrLn ("Could not resolve the host: " ++ hostname ser)
+    Right val -> connectSocket $ SockAddrInet (port ser) val
+        where
+          connectSocket = connect sock -- TODO handle failed connection
+
+  putStrLn ("Connecting...") -- Ya okay...
+
+                   
 putHelpInformation :: IO ()
 putHelpInformation = do
   putStrLn("Commands:")
   putStrLn("  c: connect")
-  putStrLn("  d: disconnect")
+--  putStrLn("  d: disconnect")
   putStrLn("  h: help (this menu)")
   putStrLn("  q: quit")
-  putStrLn("  s: show configuration") -- todo
-  putStrLn("  r: reload default configuration") -- todo
+--  putStrLn("  s: show configuration")
+--  putStrLn("  r: reload default configuration")
+
+{-| This application is a simple IRC bot.
+    It currently does not do anything.
+    The program is intended to run on a server in a terminal multiplexer such
+    as screen or tmux.
+-}
+
+{- Needed features:
+     *Read configurations from a config file.
+     *The commented out functions in the main() case of.
+     *Some sort of testing integration.
+     *fork to handle irc commands after connected to server?
+     *ncurses view of IRC output?
+-}
 
 main :: IO ()
 main = do
-  putStrLn ("Default configuration:")
-  -- putDefaultConfig()
   putStrLn ("Please enter command (h for help): ")
   line <- getLine
   case line of
+    "c"  -> connectToServer s
+--    "d"  -> disconnectFromServer
     "h"  -> putHelpInformation
     "q"  -> exitSuccess
-    _    -> return () -- do nothing
+--    "r"  -> reloadConfiguration
+--    "s"  -> putDefaultConfiguration
+    _    -> return ()
   main
